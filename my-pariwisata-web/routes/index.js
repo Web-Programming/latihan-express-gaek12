131d5f0142681399fa95e6ef69efb4358e0f9916
gaek12 <160828231+gaek12@users.noreply.github.com>
2024-11-22 17:07:04 +0700

    ughio
    uiuyi

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Beranda' });
 });
+
+router.get('/paketwisata', function(req, res, next) {
+  res.render('paketwisata', { title: 'Paket Wisata' });
+});
+router.get('/orderpaket', function(req, res, next) {
+  res.render('orderpaket', { title: 'Pemesanan Paket Wisata' });
+});
+router.get('/orderdetail', function(req, res, next) {
+  res.render('orderdetail', { title: 'Detail Pemesanan Paket Wisata' });
+});
+
 
 module.exports = router;
